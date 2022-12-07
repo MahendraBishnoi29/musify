9a97cff9ed069598d7b80c946d7cac0825eec0cf
Mahendra <mahendrabrowny@gmail.com>
2022-12-07 13:01:46 +0530

    redux toolkit api setup

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "7f20bcc9femsheb9b1d18447a039p1d45e4jsn43f1bae6971d"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
