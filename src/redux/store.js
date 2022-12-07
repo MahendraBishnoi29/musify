24fa2dc8e121e547543826b84315e10e38698177
Mahendra <mahendrabrowny@gmail.com>
2022-12-07 13:02:00 +0530

    reducers for store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import playerReducer from './features/playerSlice';
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable quotes */
+import { configureStore } from "@reduxjs/toolkit";
+import { shazamCoreApi } from "./services/shazamCore";
+import playerReducer from "./features/playerSlice";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
