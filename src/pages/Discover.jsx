cf2f7f4a2dc314e6da44086e4b3077f68053bb88
Mahendra <mahendrabrowny@gmail.com>
2022-12-11 20:08:52 +0530

    top artists avatar slider ✔🙂

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -15,11 +15,9 @@ const Discover = () => {
 
   const genreTitle = "Pop";
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">
-          Discover {genreTitle}
-        </h2>
+        <h2 className="font-bold text-3xl text-white">Discover {genreTitle}</h2>
         <select
           onChange={() => {}}
           value=""
