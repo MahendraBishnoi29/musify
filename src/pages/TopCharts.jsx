b33ad677a142bc16c5863a2aafdd36f8f05314a1
Mahendra <mahendrabrowny@gmail.com>
2022-12-15 17:44:59 +0530

    Top charts component completed.

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,42 @@
-const TopCharts = () => <div>TopCharts</div>;
+/* eslint-disable nonblock-statement-body-position */
+/* eslint-disable import/no-cycle */
+/* eslint-disable curly */
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-export default TopCharts;
+const TopArtists = () => {
+  const { isPlaying, activeSong } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader />;
+  if (isFetching && error) return <Error title="Loading Top Charts..." />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Charts
+      </h2>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data?.map((song, i) => (
+          <SongCard
+            song={song}
+            data={data}
+            key={song.key}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopArtists;
