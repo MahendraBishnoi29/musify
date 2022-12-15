0ca911ee5eaf83ffa632b4fabc1fbf77b4a17833
Mahendra <mahendrabrowny@gmail.com>
2022-12-15 17:44:06 +0530

    around you toolkit query

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -25,6 +25,9 @@ export const shazamCoreApi = createApi({
     getArtistDetails: builder.query({
       query: (artistId) => `/artists/details?artist_id=${artistId}`,
     }),
+    getAroundYouSongs: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
   }),
 });
 
@@ -33,4 +36,5 @@ export const {
   useGetSongDetailsQuery,
   useGetRelatedSongsQuery,
   useGetArtistDetailsQuery,
+  useGetAroundYouSongsQuery,
 } = shazamCoreApi;
