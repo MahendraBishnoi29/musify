66d961e3609f58cf10526473c13fbfbf5d6a2fb9
Mahendra <mahendrabrowny@gmail.com>
2022-12-16 19:27:31 +0530

    search spngs component completed 🎉

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,45 @@
-const Search = () => (
-  <div>Search</div>
-);
+/* eslint-disable nonblock-statement-body-position */
+/* eslint-disable import/no-cycle */
+/* eslint-disable curly */
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Error, Loader, SongCard } from "../components";
+import { useGetSongsBySearchQuery } from "../redux/services/shazamCore";
+
+const Search = () => {
+  const { searchTerm } = useParams();
+  const { isPlaying, activeSong } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsBySearchQuery(searchTerm);
+
+  const songs = data?.tracks?.hits?.map((song) => song.track);
+
+  if (isFetching) return <Loader />;
+  if (isFetching && error) return <Error title="Loading Top Charts..." />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Showing results for <span className="font-black">{searchTerm}</span>
+      </h2>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {songs?.map((song, i) => (
+          <SongCard
+            song={song}
+            data={data}
+            key={song.key}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
