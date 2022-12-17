32c645254856be471229ecde80eae69a59194bd0
Mahendra <mahendrabrowny@gmail.com>
2022-12-17 13:14:19 +0530

    changing app logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,4 @@
-import loader from './loader.svg';
-import logo from './logo.svg';
+import loader from "./loader.svg";
+import logo from "./musify.png";
 
-export {
-  logo,
-  loader,
-};
+export { logo, loader };
